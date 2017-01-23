b2843e912826b6fa63a8baf82d3118a1b806a12d
nihitx <nihit@Nihits-MacBook-Pro.local>
2017-01-24 00:20:25 +0200

    changed title

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
     for( var i= 0 ; i < docs.length ; i+= chunkSize) {
         productChunks.push(docs.slice(i, i+ chunkSize));
     }
-      res.render('shop/index', { title: 'Shopping Cart' , products : productChunks , successMsg : successMsg , noMessage : !successMsg});
+      res.render('shop/index', { title: 'Masnads Fashion Store' , products : productChunks , successMsg : successMsg , noMessage : !successMsg});
   });
 });
 
